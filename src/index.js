6063d8aa4ac1697c49b4473d48baa17d826d9b32
rmiyoyo <miyoyoraphael@gmail.com>
2023-09-06 21:29:24 +0300

    Add list of meals to homepage

--- src/index.js
+++ src/index.js
@@ -1,7 +1,18 @@
 import './styles/style.css';
-import './assets/close.svg';
-import popup from './module/popup.js';
+import {
+  fetchFoodInfo, enterMeals, fetchInfo, showInfo,
+} from './module/mealDetails.js';
 
-document.addEventListener('click', (e) => {
-  popup(e);
+const foodDescr = await fetchFoodInfo();
+const metricsInfo = await fetchInfo();
+enterMeals(foodDescr);
+foodDescr.forEach((element) => {
+  showInfo(metricsInfo, `M${element.idMeal}`);
+});
+
+const popupWindow = document.querySelector('.popup-window');
+popupWindow.addEventListener('click', (e) => {
+  if (e.target && e.target.matches('i.minimize-card')) {
+    popupWindow.innerHTML = '';
+  }
 });
