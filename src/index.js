dd09b73ac438573e0058b17749c67b77b62cde9b
rmiyoyo <miyoyoraphael@gmail.com>
2023-09-08 00:00:21 +0300

    Return popup

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ import fetchFoodInfo from './module/foodApi.js';
 import enterMeals from './module/foodCards.js';
 import { oneLike, fetchInfo, showInfo } from './module/like.js';
 import { mealsnumber, showMeals } from './module/mealCount';
+import { showPopup, togglePopup } from './module/popup.js';
 
 const foodDescr = await fetchFoodInfo();
 const metricsInfo = await fetchInfo();
@@ -14,6 +15,9 @@ showMeals(opinionsNo);
 foodDescr.forEach((element) => {
   showInfo(metricsInfo, `M${element.idMeal}`);
 });
+
+const comment = [...document.querySelectorAll('.comment')];
+const popWindow = document.getElementById('my-popup-window');
 
 const mealArea = document.querySelector('.menu');
 mealArea.addEventListener('click', async (e) => {
@@ -31,4 +35,13 @@ popupWindow.addEventListener('click', (e) => {
   if (e.target && e.target.matches('i.minimize-card')) {
     popupWindow.innerHTML = '';
   }
+});
+
+document.addEventListener('click', (e) => {
+  const { target } = e;
+  if (target.matches('.comment')) {
+    const { strMealThumb, strMeal } = foodDescr[comment.indexOf(target)];
+    popWindow.innerHTML = showPopup(strMealThumb, strMeal);
+    togglePopup();
+  } else if (target.matches('#close')) togglePopup();
 });
