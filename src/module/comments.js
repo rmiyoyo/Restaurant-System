90be0f6fac04cde9d2a01e9946c5376efacbc400
Stanley Osagie Ramos <sagiecyber@gmail.com>
2023-09-07 23:45:14 +0100

    Write retrieve comment and and write to DOM

--- src/module/comments.js
+++ src/module/comments.js
@@ -1,122 +1,68 @@
-const getCurrentDate = (date) => {
-  const day = date.getDate().toString().padStart(2, '0');
-  const month = (date.getMonth() + 1).toString().padStart(2, '0');
-  const year = date.getFullYear();
-
-  return `${day}/${month}/${year}`;
-};
-
 class InvolvementApiClient {
-  constructor(appId, statusElement) {
-    this.appId = appId;
-    this.baseUrl = 'https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/';
+  constructor(statusElement) {
+    this.url = 'https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/R35CzyDIJMb5HV6XSpZu/comments';
     this.statusMsg = document.querySelector(statusElement);
   }
 
-  async createComment(item_id, username, comment) {
-    const commentUrl = `${this.baseUrl}apps/${this.appId}/comments/`;
-    const currentDate = new Date();
-    const date = currentDate;
-    const commentData = {
-      date, item_id, username, comment,
+  async postComment(itemId, username, comment, successMsgSelector) {
+    const postData = {
+      itemId,
+      username,
+      comment,
     };
-
+    const successElement = document.querySelector(successMsgSelector);
     try {
-      const response = await fetch(commentUrl, {
+      const response = await fetch(this.url, {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
         },
-        body: JSON.stringify(commentData),
+        body: JSON.stringify(postData),
       });
-
-      this.statusMsg.classList.remove('hidden');
-
+      successElement.classList.remove('hidden');
       if (response.status === 201) {
-        this.statusMsg.textContent = 'Comment created successfully!';
-      } else {
-        this.statusMsg.textContent = `Failed to create a comment. Status code: ${response.status}`;
+        successElement.textContent = 'Comment posted successfully!';
+      } else if (!response.ok) {
+        successElement.textContent = `Error: ${response.status} ${response.statusText}`;
       }
-    } catch (error) {
-      this.statusMsg.textContent = 'Error creating a comment: " + error';
+    } catch (networkError) {
+      successElement.classList.remove('hidden');
+      successElement.textContent = 'There was a network problem:';
     }
+
     setTimeout(() => {
-      this.statusMsg.classList.add('hidden');
-    }, 3000);
-
-    await this.fetchComments();
+      successElement.classList.add('hidden');
+    }, 5000);
   }
 
-  async fetchComments() {
-    const commentsUrl = `${this.baseUrl}apps/${this.appId}/comments`;
-    let comments = null;
-
+  async fetchComments(itemId, successMsgSelector) {
+    const url = `${this.url}?item_id=${itemId}`;
+    const successElement = document.querySelector(successMsgSelector);
     try {
-      const response = await fetch(commentsUrl);
-      comments = await response.json();
-    } catch (error) {
-      this.statusMsg.classList.remove('hidden');
-      this.statusMsg.textContent = `Failed to fetch comments: ${error}`;
-      setTimeout(() => {
-        this.statusMsg.classList.add('hidden');
-      }, 3000);
-    }
+      const response = await fetch(url);
 
-    return comments;
-  }
-
-  async createLike(item_id) {
-    const likeUrl = `${this.baseUrl}apps/${this.appId}/likes/`;
-    const likeData = { item_id };
-
-    try {
-      const response = await fetch(likeUrl, {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify(likeData),
-      });
-
-      if (response.status === 201) {
-        console.log('Like created successfully!');
-      } else {
-        console.error(`Failed to create a like. Status code: ${response.status}`);
+      if (!response.ok) {
+        if (successElement) {
+          successElement.classList.remove('hidden');
+          successElement.textContent = 'Error fetching data';
+        }
+        throw new Error(`Error fetching data: ${response.status} ${response.statusText}`);
       }
+
+      const data = await response.json();
+      return data;
     } catch (error) {
-      console.error(`Error creating a like: ${error}`);
+      if (successElement) {
+        successElement.classList.remove('hidden');
+        successElement.textContent = 'There was a problem fetching data';
+      }
     }
-  }
 
-  async fetchLikes() {
-    const likesUrl = `${this.baseUrl}apps/${this.appId}/likes/`;
-
-    try {
-      const response = await fetch(likesUrl);
-      const likes = await response.json();
-
-      console.log('Likes:');
-      likes.forEach((like) => {
-        console.log(`Item ID: ${like.item_id}, Likes: ${like.likes}`);
-      });
-    } catch (error) {
-      console.error(`Failed to fetch likes: ${error}`);
-    }
+    setTimeout(() => {
+      if (successElement) successElement.classList.add('hidden');
+    }, 5000);
+    return null;
   }
 }
-
-/* const appId = 'abc234';
-const apiClient = new InvolvementApiClient(appId);
-
-(async () => {
-  await apiClient.createComment('item1', 'Jane', 'Hello');
-  await apiClient.fetchComments('item1');
-
-  await apiClient.createLike('item1');
-  await apiClient.fetchLikes();
-
-  await apiClient.createReservation('item1', 'Jane', '2023-09-15', '2023-09-16');
-  await apiClient.fetchReservations('item1');
-})(); */
 
 export default InvolvementApiClient;
--- src/module/popup.js
+++ src/module/popup.js
@@ -7,16 +7,13 @@ const showPopup = (img, strMeal) => {
 <img class="popup-img" src="${img}" alt="spaceship">
 <div class="sec">
   <h2 class="strMeal">${strMeal}</h2>
-  <h3>Comments (2)</h3>
-  <article class="comments">
-      <p>3/11/2021 Alex: I'd love to buy it!</p>
-      <p>03/12/2021 Mia: I love</p>
-  </article>
+  <div id="cmn"></div>
+  <article id="comments-pp"></article>
   <h3>Add a comment</h3>
   <form action="#">
       <input id="user-name" type="text" required maxlength="50" placeholder="Your name">
       <textarea id="user-comment" type="text" required maxlength="1024" placeholder="Your insights"></textarea>
-      <div id="msg" class="hidden><div>
+      <div id="msg" class="hidden"></div>
       <button class="bt" id="submit-comment" type="submit">Comment</button>
   </form>
 </div>
