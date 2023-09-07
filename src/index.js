74fadd43b57e021bb5279f323fc2a21b92615d95
rmiyoyo <miyoyoraphael@gmail.com>
2023-09-07 13:22:13 +0300

    Implement add likes feature

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import './styles/style.css';
 import './styles/mystyle.css';
 import {
-  fetchFoodInfo, enterMeals, fetchInfo, showInfo,
+  fetchFoodInfo, enterMeals, oneLike, fetchInfo, showInfo,
 } from './module/mealDetails.js';
 
 import { showPopup, togglePopup } from './module/popup.js';
@@ -16,6 +16,23 @@ foodDescr.forEach((element) => {
 
 const comment = [...document.querySelectorAll('.comment')];
 const popWindow = document.getElementById('my-popup-window');
+
+const mealArea = document.querySelector('.menu');
+mealArea.addEventListener('click', async (e) => {
+  e.preventDefault();
+  if (e.target && e.target.matches('i.heart')) {
+    const uniqueID = e.target.id;
+    await oneLike(uniqueID);
+    const metricsInfo = await fetchInfo();
+    showInfo(metricsInfo, uniqueID);
+  }
+});
+const popupWindow = document.querySelector('.popup-window');
+popupWindow.addEventListener('click', (e) => {
+  if (e.target && e.target.matches('i.minimize-card')) {
+    popupWindow.innerHTML = '';
+  }
+});
 
 document.addEventListener('click', (e) => {
   const { target } = e;
