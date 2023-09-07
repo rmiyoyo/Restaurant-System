0b5832dec2605b6f0ca4ec1284fcb9f42fb7e61e
Stanley Osagie Ramos <sagiecyber@gmail.com>
2023-09-07 01:24:11 +0100

    implement pop up form page array

--- dist/c3613bce101577dc7ba5.js
+++ dist/c3613bce101577dc7ba5.js
@@ -4,26 +4,20 @@ import {
   fetchFoodInfo, enterMeals, fetchInfo, showInfo,
 } from './module/mealDetails.js';
 
-import {showPopup, togglePopup} from './module/popup.js';
+import showPopup from './module/popup.js';
 
 const foodDescr = await fetchFoodInfo();
 const metricsInfo = await fetchInfo();
-
-const displayMeals = () => {
-  enterMeals(foodDescr);
-  foodDescr.forEach((element) => {
-    showInfo(metricsInfo, `M${element.idMeal}`);
-  });
-}
-
-displayMeals();
+enterMeals(foodDescr);
+foodDescr.forEach((element) => {
+  showInfo(metricsInfo, `M${element.idMeal}`);
+});
 
 console.log(foodDescr);
 console.log(metricsInfo);
 
 const comment = [...document.querySelectorAll('.comment')];
-const popWindow = document.getElementById('my-popup-window');
-const menu = document.querySelector('.menu');
+const popWindow = document.querySelector('popup-window');
 
 document.addEventListener('click', (e) => {
   const { target } = e;
@@ -33,11 +27,7 @@ document.addEventListener('click', (e) => {
   if (target.matches('.comment')) {
     const index = comment.indexOf(target);
     const food = foodDescr[index];
-    console.log(food);
-    const { strMealThumb, strMeal } = food;
-    popWindow.innerHTML = showPopup(strMealThumb, strMeal);
-    togglePopup();
-  } else if(target.matches('#close')) {
-    togglePopup();
+    console.log(index);
+    showPopup(food.strMealThumb);
   }
 });
--- src/module/popup.js
+++ src/module/popup.js
@@ -1,21 +1,11 @@
 import closeIcon from '../assets/close.svg';
-import demoSpaceship from '../assets/demoSpaceship.webp';
 
-const popupContent = `
+const showPopup = (img, strMeal) => {
+  const popupContent = `
 <section id="popup">
 <img  id="close" src="${closeIcon}" alt="close">
-<img class="popup-img" src="${demoSpaceship}" alt="spaceship">
-<h2 class="ship-name">Space 3</h2>
-<article class="ship-properties">
-    <div>
-        <p>Fuel: Titanuim</p>
-        <p>Weight: 400</p>
-    </div>
-    <div>
-        <p>Length: 100,000</p>
-        <p>Power: 100,000,000</p>
-    </div>
-</article>
+<img class="popup-img" src="${img}" alt="spaceship">
+<h2 class="strMeal">${strMeal}</h2>
 <h3>Comments (2)</h3>
 <article class="comments">
     <p>3/11/2021 Alex: I'd love to buy it!</p>
@@ -29,25 +19,19 @@ const popupContent = `
 </form>
 </section>
 `;
-
-const popupConatiner = document.getElementById('popup-container');
-const header = document.querySelector('header');
-let popupMonitor = null;
-
-const popup = (e) => {
-  const { target } = e;
-  if (target.id === 'close') {
-    while (popupConatiner.firstChild) {
-      popupConatiner.firstChild.remove();
-      popupMonitor.classList.toggle('displayNone');
-      header.classList.toggle('displayNone');
-    }
-  } else if (target.id === 'popup-demo') {
-    popupConatiner.innerHTML = popupContent;
-    target.classList.toggle('displayNone');
-    header.classList.toggle('displayNone');
-    popupMonitor = target;
-  }
+  return popupContent;
 };
 
-export default popup;
+const toggleHiddenList = [
+  document.querySelector('.menu'),
+  document.querySelector('header'),
+  document.getElementById('my-popup-window'),
+];
+
+const togglePopup = () => {
+  toggleHiddenList.forEach((element) => {
+    element.classList.toggle('hidden');
+  });
+};
+
+export { showPopup, togglePopup };
