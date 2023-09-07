c490c315c78d8fffb685867e2879123911343768
rmiyoyo <miyoyoraphael@gmail.com>
2023-09-07 18:42:26 +0300

    Add meals counter function to index.js

--- src/index.js
+++ src/index.js
@@ -1,21 +1,19 @@
 import './styles/style.css';
 import './styles/mystyle.css';
-import {
-  fetchFoodInfo, enterMeals, oneLike, fetchInfo, showInfo,
-} from './module/mealDetails.js';
 
-import { showPopup, togglePopup } from './module/popup.js';
+import fetchFoodInfo from './module/foodApi.js';
+import enterMeals from './module/foodCards.js';
+import { oneLike, fetchInfo, showInfo } from './module/like.js';
+import { mealsnumber, showMeals } from './module/mealCount';
 
 const foodDescr = await fetchFoodInfo();
 const metricsInfo = await fetchInfo();
-
 enterMeals(foodDescr);
+const opinionsNo = await mealsnumber();
+showMeals(opinionsNo);
 foodDescr.forEach((element) => {
   showInfo(metricsInfo, `M${element.idMeal}`);
 });
-
-const comment = [...document.querySelectorAll('.comment')];
-const popWindow = document.getElementById('my-popup-window');
 
 const mealArea = document.querySelector('.menu');
 mealArea.addEventListener('click', async (e) => {
@@ -27,18 +25,10 @@ mealArea.addEventListener('click', async (e) => {
     showInfo(metricsInfo, uniqueID);
   }
 });
+
 const popupWindow = document.querySelector('.popup-window');
 popupWindow.addEventListener('click', (e) => {
   if (e.target && e.target.matches('i.minimize-card')) {
     popupWindow.innerHTML = '';
   }
-});
-
-document.addEventListener('click', (e) => {
-  const { target } = e;
-  if (target.matches('.comment')) {
-    const { strMealThumb, strMeal } = foodDescr[comment.indexOf(target)];
-    popWindow.innerHTML = showPopup(strMealThumb, strMeal);
-    togglePopup();
-  } else if (target.matches('#close')) togglePopup();
 });
